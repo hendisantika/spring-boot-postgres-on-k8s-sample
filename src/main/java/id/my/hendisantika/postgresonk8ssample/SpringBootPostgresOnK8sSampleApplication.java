58eb8e94f274f1fa6e4afd87b6d60a3d87825d01
hendisantika <hendisantika@yahoo.co.id>
2025-05-08 06:49:32 +0700

    feat: Init Project🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/postgresonk8ssample/SpringBootPostgresOnK8sSampleApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.postgresonk8ssample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPostgresOnK8sSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootPostgresOnK8sSampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/postgresonk8ssample/SpringBootPostgresOnK8sSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.postgresonk8ssample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPostgresOnK8sSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
