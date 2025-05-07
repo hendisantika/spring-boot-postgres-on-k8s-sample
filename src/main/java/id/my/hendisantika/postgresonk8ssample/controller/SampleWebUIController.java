da79a78547b46b628e1389e2762f9b9dd1511ff9
hendisantika <hendisantika@yahoo.co.id>
2025-05-08 06:54:38 +0700

    feat: Add showValue in SampleWebUIController🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/postgresonk8ssample/controller/SampleWebUIController.java
+++ src/main/java/id/my/hendisantika/postgresonk8ssample/controller/SampleWebUIController.java
@@ -1,9 +1,14 @@
 package id.my.hendisantika.postgresonk8ssample.controller;
 
+import id.my.hendisantika.postgresonk8ssample.entity.Value;
 import id.my.hendisantika.postgresonk8ssample.repository.ValueRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -23,5 +28,10 @@ public class SampleWebUIController {
 
     private final ValueRepository repository;
 
+    @GetMapping
+    public ModelAndView showValue() {
+        List<Value> values = (List<Value>) repository.findAll();
+        return new ModelAndView("index", "value", values.isEmpty() ? null : values.get(0).getValue());
+    }
 
 }
