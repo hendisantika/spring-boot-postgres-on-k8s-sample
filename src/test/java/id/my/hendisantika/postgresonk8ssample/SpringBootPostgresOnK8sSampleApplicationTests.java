cc2ddbaf8b6cbe88d1513e65ba56eaf578143f09
hendisantika <hendisantika@yahoo.co.id>
2025-05-08 10:33:45 +0700

    feat: Add TestContainers Test🫸🌀✏️📗🐧🐳

--- src/test/java/id/my/hendisantika/postgresonk8ssample/SpringBootPostgresOnK8sSampleApplicationTests.java
+++ src/test/java/id/my/hendisantika/postgresonk8ssample/SpringBootPostgresOnK8sSampleApplicationTests.java
@@ -2,12 +2,20 @@ package id.my.hendisantika.postgresonk8ssample;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.containers.PostgreSQLContainer;
 
 @SpringBootTest
 class SpringBootPostgresOnK8sSampleApplicationTests {
 
     @Test
     void contextLoads() {
+        try (PostgreSQLContainer<?> postgresContainer = new PostgreSQLContainer<>("postgres:17.4")) {
+            postgresContainer.start();
+
+            System.out.println("PostgreSQL URL: " + postgresContainer.getJdbcUrl());
+            System.out.println("Username: " + postgresContainer.getUsername());
+            System.out.println("Password: " + postgresContainer.getPassword());
+        }
     }
 
 }
